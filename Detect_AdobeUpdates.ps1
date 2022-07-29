d81a150bd00aacebab646eaf030e1295431913f2
virtualtech516 <56199055+virtualtech516@users.noreply.github.com>
2022-07-29 18:01:18 -0400

    Add files via upload

---
+++ Detect_AdobeUpdates.ps1
@@ -0,0 +1,19 @@
+try {
+
+$cmdPath = "${env:ProgramFiles(x86)}\Common Files\Adobe\OOBE_Enterprise\RemoteUpdateManager\RemoteUpdateManager.exe"
+ 
+    if (Test-Path -Path $cmdPath){
+        Write-Host "Adobe RUM Found"
+        exit 1
+    }
+    else
+   {
+        Write-Host "Not Found"
+        exit 0
+    }   
+}
+catch {
+    $errMsg = $_.Exception.Message
+    Write-Error $errMsg
+    exit 1
+}
