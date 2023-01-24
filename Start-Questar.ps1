d47fc8497de91d614cdc7ae0050fedafba5af95a
virtualtech516 <56199055+virtualtech516@users.noreply.github.com>
2023-01-23 21:50:58 -0500

    Delete Start-Questar.ps1

--- Start-Questar.ps1
+++
@@ -1,4 +0,0 @@
-﻿Write-Host "Closing Microsoft Teams & Edge"
-Get-Process -Name Teams, msedge -ErrorAction SilentlyContinue | Stop-Process
-Write-Host "Starting Questar Secure Browser"
-Start-Process -NoNewWindow -FilePath "C:\Program Files (x86)\Questar Assessment Inc\QuestarStudent-NY\QuestarStudent-NY.exe" -WorkingDirectory "C:\Program Files (x86)\Questar Assessment Inc\QuestarStudent-NY\"
