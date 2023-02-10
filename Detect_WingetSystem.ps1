98d1188776144b0010919af1f8ce694775d92f09
virtualtech516 <56199055+virtualtech516@users.noreply.github.com>
2023-02-10 10:08:31 -0500

    Add files via upload

--- Detect_WingetSystem.ps1
+++ Detect_WingetSystem.ps1
@@ -4,34 +4,58 @@ $App3 = "DominikReichl.KeePass"
 $App4 = "WiresharkFoundation.Wireshark"
 $App5 = "VideoLAN.VLC"
 $App6 = "Audacity.Audacity"
+$App7 = "JetBrains.IntelliJIDEA.Community"
+$App8 = "Notepad++.Notepad++"
+$App9 = "BlenderFoundation.Blender"
+
+#$App = "Microsoft.VCRedist.2015+.x86"
+#$App = "Microsoft.VCRedist.2015+.x64"
+#$App = "Microsoft.SQLServer2012NativeClient"
+#$App = "Microsoft.VCRedist.2013.x86"
+#$App = "Microsoft.VCRedist.2013.x64"
+
 
 $Winget = Get-ChildItem -Path (Join-Path -Path (Join-Path -Path $env:ProgramFiles -ChildPath "WindowsApps") -ChildPath "Microsoft.DesktopAppInstaller*_x64*\winget.exe")
+$Upgrade = &$winget upgrade --scope machine --accept-source-agreements | Out-String
 
-if ($(&$winget upgrade --accept-source-agreements) -like "* $App1 *") {
+if ($Upgrade.Contains($App1)) {
 	Write-Host "Upgrade available for: $App1"
-	exit 1 # upgrade available, remediation needed
+	exit 1
 }
-elseif ($(&$winget upgrade --accept-source-agreements) -like "* $App2 *") {
+elseif ($Upgrade.Contains($App2)) {
 	Write-Host "Upgrade available for: $App2"
-	exit 1 # upgrade available, remediation needed
+	exit 1
 }
-elseif ($(&$winget upgrade --accept-source-agreements) -like "* $App3 *") {
+elseif ($Upgrade.Contains($App3)) {
 	Write-Host "Upgrade available for: $App3"
-	exit 1 # upgrade available, remediation needed
+	exit 1
 }
-elseif ($(&$winget upgrade --accept-source-agreements) -like "* $App4 *") {
+elseif ($Upgrade.Contains($App4)) {
 	Write-Host "Upgrade available for: $App4"
-	exit 1 # upgrade available, remediation needed
+	exit 1
 }
-elseif ($(&$winget upgrade --accept-source-agreements) -like "* $App5 *") {
+elseif ($Upgrade.Contains($App5)) {
 	Write-Host "Upgrade available for: $App5"
-	exit 1 # upgrade available, remediation needed
+	exit 1
 }
-elseif ($(&$winget upgrade --accept-source-agreements) -like "* $App6 *") {
+elseif ($Upgrade.Contains($App6)) {
 	Write-Host "Upgrade available for: $App6"
-	exit 1 # upgrade available, remediation needed
+	exit 1
 }
+elseif ($Upgrade.Contains($App7)) {
+	Write-Host "Upgrade available for: $App7"
+	exit 1
+}
+elseif ($Upgrade.Contains($App8)) {
+	Write-Host "Upgrade available for: $App8"
+	exit 1
+}
+elseif ($Upgrade.Contains($App9)) {
+	Write-Host "Upgrade available for: $App9"
+	exit 1
+}
+
 else {
 	Write-Host "No Upgrade available"
-	exit 0 # no upgrade, no action needed
+	exit 0
 }
---
+++ Detect_WingetUser.ps1
@@ -0,0 +1,22 @@
+﻿$App1 = "Microsoft.VisualStudioCode"
+$App2 = "Zoom.Zoom"
+$App3 = "Microsoft.WindowsTerminal"
+
+$Upgrade = winget upgrade --scope user --accept-source-agreements | Out-String
+
+if ($Upgrade.Contains($App1)) {
+	Write-Host "Upgrade available for: $App1"
+	exit 1
+}
+elseif ($Upgrade.Contains($App2)) {
+	Write-Host "Upgrade available for: $App2"
+	exit 1
+}
+elseif ($Upgrade.Contains($App3)) {
+	Write-Host "Upgrade available for: $App3"
+	exit 1
+}
+else {
+	Write-Host "No Upgrade available"
+	exit 0
+}
