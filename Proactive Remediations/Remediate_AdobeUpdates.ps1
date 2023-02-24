f0c49a9c4793e1d1d0e42e500d1487195d825194
HankMardukasNY <56199055+HankMardukasNY@users.noreply.github.com>
2023-02-24 15:42:48 -0500

    Rename Remediate_AdobeUpdates.ps1 to Proactive Remediations/Remediate_AdobeUpdates.ps1

---
+++ Proactive Remediations/Remediate_AdobeUpdates.ps1
@@ -0,0 +1,28 @@
+try {
+
+$cmdPath = "${env:ProgramFiles(x86)}\Common Files\Adobe\OOBE_Enterprise\RemoteUpdateManager\RemoteUpdateManager.exe"
+$cmdArgList = @(
+	"--action=install"
+)
+
+& $cmdPath $cmdArgList
+
+    if($LASTEXITCODE -eq 0){
+    Write-Host "Adobe Apps Updated"
+    exit 0
+    }
+    if($LASTEXITCODE -eq 1){
+    Write-Host "Generic error"
+    exit 1
+    }
+    if($LASTEXITCODE -eq 2){
+    Write-Host "One or more updates could not be installed"
+    exit 1
+    }
+}
+catch {
+
+    $errMsg = $_.Exception.Message
+    Write-Error $errMsg
+    exit 1
+}
